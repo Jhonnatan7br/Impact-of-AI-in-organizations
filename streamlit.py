f21930e582731f792067d7d50b8e769ebcc1a6c1
Jhonnatan7br <jhonnatan7br@gmail.com>
2024-01-22 11:25:55 +0100

    Updated

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,25 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
-st.write("""
-# My first app
-Hello *world!*
-""")
- 
-df = pd.read_csv("scopus.csv")
-st.line_chart(df)
+# Title of the app
+st.title('Publications per Year - Line Graph')
+
+# File uploader to allow users to upload their own CSV file
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+
+    # Processing the data to count the number of publications per year
+    publications_per_year = data['Year'].value_counts().sort_index()
+
+    # Creating a line graph
+    plt.figure(figsize=(10, 6))
+    plt.plot(publications_per_year.index, publications_per_year.values, marker='o')
+    plt.xlabel('Year')
+    plt.ylabel('Number of Publications')
+    plt.title('Number of Publications per Year')
+    plt.grid(True)
+    st.pyplot(plt.gcf())
+else:
+    st.write("Please upload a CSV file to display the graph.")
