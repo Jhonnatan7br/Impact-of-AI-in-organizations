c11bd7fd5f0079c4d1136b898e5ddbacac319479
Jhonnatan7br <jhonnatan7br@gmail.com>
2024-01-26 16:50:26 +0100

    Updated

--- LDA_topic_modeling.py
+++ LDA_topic_modeling.py
@@ -17,10 +17,13 @@ import pyLDAvis.gensim_models as gensimvis
 import pyLDAvis
 import matplotlib.pyplot as plt
 
+import pyLDAvis.gensim_models as gensimvis
+import pyLDAvis
+
 research = pd.read_csv("C:/Users/Jhonnatan/Documents/GitHub/Impact-of-AI-in-organizations/Datasets/scopus.csv")
 
 # Create a sub-dataset with the first 10 lines
-sub_dataset = research.head(10)
+sub_dataset = research.head(1000)
 # Extract descriptions from the 'description' column of the dataframe
 text_corpus = sub_dataset['Abstract'].tolist()
 text_corpus = [f'"{doc}"' for doc in text_corpus]
@@ -57,54 +60,15 @@ corpus = [dictionary.doc2bow(text) for text in processed_corpus]
 common_dictionary = Dictionary(common_texts)
 common_corpus = [common_dictionary.doc2bow(text) for text in common_texts]
 # Train the LDA model on the BoW corpus.
-lda = LdaModel(corpus, num_topics=10)
+#lda = LdaModel(corpus, num_topics=10)
 # Train the LDA model
-lda_model = LdaModel(corpus, num_topics=2, id2word=dictionary, passes=15)
+lda_model = LdaModel(corpus, num_topics=10, id2word=dictionary, passes=15)
 #lda = models.LdaModel(corpus, num_topics=10)
 
-
-#%%
-
-documents = [
-    "Machine learning is a subset of artificial intelligence.",
-    "Natural language processing is important in NLP tasks.",
-    "Topic modeling is a technique for discovering hidden topics.",
-    "Gensim is a Python library for natural language processing.",
-    "Latent Dirichlet Allocation is an unsupervised learning algorithm.",
-]
-
-import gensim
-from gensim import corpora
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import string
-
-# Tokenize and preprocess the documents
-stop_words = set(stopwords.words('english'))
-translator = str.maketrans('', '', string.punctuation)
-
-tokenized_documents = []
-for doc in documents:
-    tokens = word_tokenize(doc.lower())  # Tokenize and convert to lowercase
-    tokens = [token for token in tokens if token not in stop_words]  # Remove stopwords
-    tokens = [token.translate(translator) for token in tokens]  # Remove punctuation
-    tokenized_documents.append(tokens)
-
-# Create a dictionary and a document-term matrix
-dictionary = corpora.Dictionary(tokenized_documents)
-corpus = [dictionary.doc2bow(doc) for doc in tokenized_documents]
-
-
-# Train the LDA model
-lda_model = gensim.models.LdaModel(corpus, num_topics=2, id2word=dictionary, passes=15)
-
-
-import pyLDAvis.gensim_models as gensimvis
-import pyLDAvis
 
 # Create a visualization
 vis_data = gensimvis.prepare(lda_model, corpus, dictionary)
 pyLDAvis.display(vis_data)
 
+#%%
 
-# %%
