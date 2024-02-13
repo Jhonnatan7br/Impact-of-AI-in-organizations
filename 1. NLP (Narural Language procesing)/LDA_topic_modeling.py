54dbfbeed92d5c60ab4f66bf982d9138395faf62
Jhonnatan7br <jhonnatan7br@gmail.com>
2024-02-13 13:49:45 +0100

    Updated

--- 1. NLP (Narural Language procesing)/LDA_topic_modeling.py
+++ 1. NLP (Narural Language procesing)/LDA_topic_modeling.py
@@ -18,62 +18,61 @@ from gensim.corpora.dictionary import Dictionary
 from gensim.models import LdaModel
 import pyLDAvis.gensim_models as gensimvis
 import pyLDAvis
-
 from nltk.corpus import stopwords
 import nltk
 nltk.download('stopwords')
-
 import spacy
 nlp = spacy.load('en_core_web_sm')
 import matplotlib.pyplot as plt
 import pyLDAvis.gensim_models as gensimvis
 import pyLDAvis
 
+""""Data: AI Business related research"""
 
 research = pd.read_csv("C:/Users/Jhonnatan/Documents/GitHub/Impact-of-AI-in-organizations/Datasets/scopus.csv")
 # Create a sub-dataset with the first 10 lines
-sub_dataset = research.head(5251)
+sub_dataset = research
 # Extract descriptions from the 'description' column of the dataframe
 text_corpus = sub_dataset['Abstract'].tolist()
 text_corpus = [f'"{doc}"' for doc in text_corpus]
 
+#%%
+""" Clean words matrix """
 # Create a set of stopwords (set of frequent words):
 #stop_words = set(stopwords.words('english'))  # nltk stopwords in english for English 
 stop_words = set(nlp.Defaults.stop_words)
 # Add manually another stop word to reduce model noise
-new_stop_words = {'ai','(ai)','patien','patients','=','de','control','model','system','et','results','power','±','new','compared','risk','data','smart','la','abstract','om','“no','available','set','problem','teatures','siven','class','rights','general','cows','milk','relevant','reserved."','time','pattern','constraint','classiters','vve I','problems','consider','propositional','logic','present','space','large','springer','prove','intake','la','©','found','problem','features','given','p','(p)','knowledge','results','si','d','"[no','available]"','use','field','"The','provide','based','paper','propose','decision','2021','process','methods','paper,','however','number','studies','study','<','conclusion:','2','1','significant','included','total','(n','des','les','genetic','à','en','le','une','qui','du','associated','literature','review',',','intelligence','artificial', 'approach','proposed','intelligence','accuracy','parameters','group','methods:','results:','(p','low', 'different', 'higher', 'analysis', 'published', 'articles','coding', 'dans', 'un', 'video', 'se', 'que', 'productivity', 'pour', 'elsevier','automatic','levels', 'expression', 'increased', 'effect', 'days', 'high','however','index', 'significantly', 'af', 'function'}
+new_stop_words = {'&','=','de','control','model','system','et','results','power','±','new','compared','risk','data','smart','la','abstract','om','“no','available','set','problem','teatures','siven','class','rights','general','cows','milk','relevant','reserved."','time','pattern','constraint','classiters','vve I','problems','consider','propositional','logic','present','space','large','springer','prove','intake','la','©','found','problem','features','given','p','(p)','knowledge','results','si','d','"[no','available]"','use','field','"The','provide','based','paper','propose','decision','2021','process','methods','paper,','however','number','studies','study','<','conclusion:','2','1','significant','included','total','(n','des','les','genetic','à','en','le','une','qui','du','associated','literature','review',',','intelligence','artificial', 'approach','proposed','intelligence','accuracy','parameters','group','methods:','results:','(p','low', 'different', 'higher', 'analysis', 'published', 'articles','coding', 'dans', 'un', 'video', 'se', 'que', 'productivity', 'pour', 'elsevier','automatic','levels', 'expression', 'increased', 'effect', 'days', 'high','however','index', 'significantly', 'af', 'function'}
 # Add the new words to the existing set
 stop_words.update(new_stop_words)
 # Building set of stop_words manually
 #stoplist = set('for a of the and to in'.split(' '))
-
 # Lowercase each document, split it by white space and filter out stopwords
 texts = [[word for word in document.lower().split() if word not in stop_words]
          for document in text_corpus]
 
+
+#%%
+""" Build frame of words to being processed on LDA"""
 # Count word frequencies
 from collections import defaultdict
 frequency = defaultdict(int)
 for text in texts:
     for token in text:
         frequency[token] += 1 # Increase frecuency Token or decrease depending espected results
-
 # Only keep words that appear more than once
 processed_corpus = [[token for token in text if frequency[token] > 1] for text in texts]
 pprint.pprint(processed_corpus)
-
-
 # Attempting to directly pass processed_corpus to LdaModel without first converting it to a Bag-of-Words (BoW) format using the doc2bow method for each document
-
 # Create Dictionary for processed corpus
 dictionary = corpora.Dictionary(processed_corpus)
 print(dictionary)
-
 # Convert the dictionary to a bag-of-words corpus for reference.
 corpus = [dictionary.doc2bow(text) for text in processed_corpus]
 # Concept similare
 
 """ Train an LDA model using a Gensim corpus """
+
 # Create a corpus from a list of texts
 common_dictionary = Dictionary(common_texts)
 common_corpus = [common_dictionary.doc2bow(text) for text in common_texts]
@@ -83,7 +82,8 @@ common_corpus = [common_dictionary.doc2bow(text) for text in common_texts]
 lda_model = LdaModel(corpus, num_topics=10, id2word=dictionary, passes=30)
 #lda = models.LdaModel(corpus, num_topics=10)
 
-# Print LDA Topic Modeling vector
+#%%
+""" Print LDA Topic Modeling vector """
 topics = lda_model.print_topics(num_words=10)
 for topic in topics:
     print(topic)
